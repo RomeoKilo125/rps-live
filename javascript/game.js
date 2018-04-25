658ff633ee93b1a1864609e6e659f8877ea3e6fd
RomeoKilo125 <romeokilo125@gmail.com>
2018-04-25 18:01:39 -0400

    got players coordinated

--- javascript/game.js
+++ javascript/game.js
@@ -10,50 +10,77 @@ game = function() {
   };
   firebase.initializeApp(config);
 
+  let playerID = "";
   let database = firebase.database();
   let weapons = database.ref('/weapons');
   let score = database.ref('/score');
   let connections = database.ref('/connections');
+  let users = database.ref('/users');
   let connected = database.ref('.info/connected');
   let numOfPlayers = 0;
   let myScore = 0;
   let theirScore = 0;
-  let playerID = "";
 
   getPlayerID = function() {
-    connections.once('value', function(snap) {
-      playerID = snap.numChildren().toString();
+    users.once('value', function(snap) {
+      if (snap.val() && snap.val().player1.status) {
+        playerID = 'player2';
+      } else {
+        playerID = 'player1';
+      }
       console.log("Player ID: " + playerID);
       score.child(playerID).set(myScore);
+      weapons.child(playerID).set('');
+    }).then(function() {
+      let statusRef = database.ref('/users/' + playerID + "/status");
+      statusRef.set(true);
+      statusRef.onDisconnect().set(false);
     });
   }
 
   startGame = function() {
+    console.log('startgame');
     myScore = 0;
     theirScore = 0;
 
     $('#score').text("YOU " + myScore + " : " + theirScore + " THEM");
 
     weapons.set({
-      1: '',
-      2: ''
+      'player1': '',
+      'player2': ''
     });
   }
 
   connected.on('value', function(snapshot) {
     if (snapshot.val()) {
+
       let con = connections.push(true);
-      getPlayerID();
+
+      connections.once('value', function(snapshot) {
+        if (snapshot.numChildren() <= 2) {
+          getPlayerID();
+        }
+      });
+
       con.onDisconnect().remove();
     }
   });
 
-  connections.on('value', function(snapshot) {
-    numOfPlayers = snapshot.numChildren();
-    numOfPlayers === 2 ? startGame() : '';
+  users.on('value', function(snapshot) {
+    console.log(playerID);
+    if (playerID && snapshot.val().player1.status && snapshot.val().player2.status) {
+      startGame();
+    }
+  });
+
+  weapons.on('value', function(snapshot) {
+    
   });
 
   $('.weapon').on('click', function(event) {
+    if (!playerID) {
+      return
+    }
     let selectedWeapon = $(this).attr('id');
     console.log(selectedWeapon);
     weapons.child(playerID).set(selectedWeapon);
