456928680c2683af66fbdf4077136cf2463743d8
RomeoKilo125 <romeokilo125@gmail.com>
2018-04-28 01:24:26 -0400

    added keyup event so pressing enter will send chat.

--- javascript/chat.js
+++ javascript/chat.js
@@ -7,15 +7,15 @@ chat = function(id) {
   let chatArray = []
   startChat = function() {
     chatArray = [];
-    chat.set(chatArray.join('|'));
+    chat.set('');
     $('#chatInput').val('');
     $('#chatArea').empty();
   }
 
   $('#btnSubmit').on("click", function(event) {
     event.preventDefault();
-    let msg = $('#chatInput').val()
-    chatArray.push(id + ": " + msg)
+    let msg = $('#chatInput').val().trim();
+    chatArray.push(id + ": " + msg);
     chat.set(chatArray.join('|'));
 
     $('#chatInput').val('');
@@ -23,10 +23,13 @@ chat = function(id) {
   });
 
   chat.on('value', function(shot) {
+    chatArray = shot.val().split('|');
     let lastMsg = shot.val().split('|').pop();
     $('#chatArea').append(lastMsg + "<br />");
   });
 
-  
+  $(document).on('keyup', function(e) {
+    e.keyCode === 13 ? $('#btnSubmit').trigger('click') : '';
+  });
 
 }
